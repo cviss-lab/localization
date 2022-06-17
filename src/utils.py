c7dc2cbd581136ea7d0625bc6190ffa093de42fb
zaid <zaalsabb@uwaterloo.ca>
2022-06-17 13:19:13 -0400

    big update

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
+import os
+import shutil
 import cv2
 import cv2.aruco as aruco
 import numpy as np
 from geometry_msgs.msg import Transform, TransformStamped
 from scipy.spatial.transform import Rotation
+
+def make_dir(path,delete_if_exists=False):   
+    if not os.path.exists(path):
+        os.makedirs(path)
+    elif delete_if_exists:
+        shutil.rmtree(path)
+        os.makedirs(path)
 
 def pinhole_model(pose,K):
     """Loads projection and extrensics information for a pinhole camera model
@@ -87,6 +96,19 @@ def ProjectToWorld(projectionMatrix, uv, R, C):
     return rays
 
 
+def Tmatrix_inverse(Tmat):
+    if Tmat.shape == (4,4):
+        R = Tmat[:3,:3]
+        t = Tmat[:3,3]
+        R_inv = R.T
+        t_inv = -R_inv.dot(t)     
+        Tmat_inv = np.eye(4)
+        Tmat_inv[:3,:3] = R_inv
+        Tmat_inv[:3,3] = t_inv
+        return Tmat_inv
+    else:
+        raise 'Error: input should be 4x4 transformation matrix'
+
 def create_transform(p,q):
     
     T = Transform()
@@ -135,6 +157,7 @@ def points2numpy(pl):
 def quaterions2numpy(ql):
     return np.array([[q.x,q.y,q.z,q.w] for q in ql])    
 
+
 def project_2d_to_3d(m,K,D,center=False, h=0):
     u = m[0,:]
     v = m[1,:]
@@ -160,11 +183,21 @@ def project_2d_to_3d(m,K,D,center=False, h=0):
     X = Z*(u-cx)/fx
     Y = Z*(v-cy)/fy    
 
-    return X,Y,Z    
+    return X,Y,Z      
 
+def countour2mask(m,I):
+    
+    mask = np.zeros(I.shape)
+    contours=m
+    mask=cv2.drawContours(mask, contours, -1, (255),1)    
+    return mask
 
+def cloud_inside_polygon(m,K,D):
+    mask = countour2mask(m,D)
+    cloud = project_2d_to_3d(mask,K,D)
+    return cloud
 
-def detect_markers(I,find_ids=None,xy_array=False):
+def detect_markers(I,find_ids=None,xy_array=False,ignore_zeros=True):
 
     gray = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY)
 
@@ -173,22 +206,22 @@ def detect_markers(I,find_ids=None,xy_array=False):
 
     # define names of each possible ArUco tag OpenCV supports
     ARUCO_DICT = {
-        "DICT_4X4_50": cv2.aruco.DICT_4X4_50,
-        "DICT_4X4_100": cv2.aruco.DICT_4X4_100,
-        "DICT_4X4_250": cv2.aruco.DICT_4X4_250,
-        "DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
-        "DICT_5X5_50": cv2.aruco.DICT_5X5_50,
-        "DICT_5X5_100": cv2.aruco.DICT_5X5_100,
-        "DICT_5X5_250": cv2.aruco.DICT_5X5_250,
-        "DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
-        "DICT_6X6_50": cv2.aruco.DICT_6X6_50,
-        "DICT_6X6_100": cv2.aruco.DICT_6X6_100,
-        "DICT_6X6_250": cv2.aruco.DICT_6X6_250,
-        "DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
-        "DICT_7X7_50": cv2.aruco.DICT_7X7_50,
-        "DICT_7X7_100": cv2.aruco.DICT_7X7_100,
-        "DICT_7X7_250": cv2.aruco.DICT_7X7_250,
-        "DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
+        # "DICT_4X4_50": cv2.aruco.DICT_4X4_50,
+        # "DICT_4X4_100": cv2.aruco.DICT_4X4_100,
+        # "DICT_4X4_250": cv2.aruco.DICT_4X4_250,
+        # "DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
+        # "DICT_5X5_50": cv2.aruco.DICT_5X5_50,
+        # "DICT_5X5_100": cv2.aruco.DICT_5X5_100,
+        # "DICT_5X5_250": cv2.aruco.DICT_5X5_250,
+        # "DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
+        # "DICT_6X6_50": cv2.aruco.DICT_6X6_50,
+        # "DICT_6X6_100": cv2.aruco.DICT_6X6_100,
+        # "DICT_6X6_250": cv2.aruco.DICT_6X6_250,
+        # "DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
+        # "DICT_7X7_50": cv2.aruco.DICT_7X7_50,
+        # "DICT_7X7_100": cv2.aruco.DICT_7X7_100,
+        # "DICT_7X7_250": cv2.aruco.DICT_7X7_250,
+        # "DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
         "DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL
         # "DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
         # "DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
@@ -198,6 +231,7 @@ def detect_markers(I,find_ids=None,xy_array=False):
 
     # loop over the types of ArUco dictionaries
     for (arucoName, arucoDict) in ARUCO_DICT.items():
+        
         # load the ArUCo dictionary, grab the ArUCo parameters, and
         # attempt to detect the markers for the current dictionary
         arucoDict = cv2.aruco.Dictionary_get(arucoDict)
@@ -207,10 +241,11 @@ def detect_markers(I,find_ids=None,xy_array=False):
             gray, arucoDict, parameters=arucoParams)    
 
         if ids is not None:
+            # print(arucoName)
             break
 
     if ids is None:
-        return None
+        return []
     ids = list(ids.reshape(-1))
 
     # if specified, only find markers with ids in find_ids list
@@ -231,9 +266,13 @@ def detect_markers(I,find_ids=None,xy_array=False):
         if con.shape[0] < 3:
             continue
 
+        if ignore_zeros and ids==0:
+            continue
+
         obj = dict([])
         obj['id'] = 'ID:'+str(i)
-        
+        obj['confidence'] = 1.0
+
         if xy_array:
             coords = np.array([[],[]])
             for pt in con:
@@ -250,5 +289,41 @@ def detect_markers(I,find_ids=None,xy_array=False):
                 obj['coords'].append(coords)
 
         objects.append(obj)     
+
+    return objects
+
+
+def detect_manual(I,m,s,h=0.05):
+
+    W = int(I.shape[1])
+    H = int(I.shape[0])
+
+    objects = []
+
+    obj = dict([])
+    obj['id'] = 'ID:'+str(s)
+    obj['confidence'] = 1.0
+
+    coords = np.array([[],[]])
+    for i in range(4):
+        if i == 2:
+            h1 = 1
+            h2 = -1
+        elif i == 1:
+            h1 = 1
+            h2 = 1
+        elif i == 0:
+            h1 = -1
+            h2 = 1
+        elif i == 3:
+            h1 = -1
+            h2 = -1                                    
+        x = (float(m[1])+h1*h)*W
+        y = (float(m[0])+h2*h)*H
+        coords = np.hstack([coords,[[x],[y]]])
+    obj['coords'] = coords
+
+
+    objects.append(obj)     
 
     return objects
